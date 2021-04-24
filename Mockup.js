6e896fd754d2bd521a37296f6aac47b07dacb793
gowthamiedamalapati <gouthami1812@gmail.com>
2021-04-24 23:08:15 +0530

    result-1

--- Mockup.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector(".menu-list");
-const menuBtn = document.querySelector(".menu-btn");
-const cancelBtn = document.querySelector(".cancel-btn");
-menuBtn.onClick = ()=>{
-    menu.classList.add("active");
-}
